b2e04a9c8f69b34bba46fee756ddee984783b3da
Sr.Bala <sr.bala.kkl@gmail.com>
2024-12-13 14:55:36 +0530

    doc: index seq

--- GeneralCommands.sql
+++ GeneralCommands.sql
@@ -1,10 +1,27 @@
--- Postgres command to insert the values to the CSV file.
+-- **** Postgres command to insert the values to the CSV file. ****
 COPY mas.panchayats(district_id,type,name)
     FROM '/var/lib/pgsql/corporation_districts.csv'-- <- if CSV file is not avail in this location then the permission error may be arrived.
     DELIMITER ','
     CSV HEADER;
 
+
+
 -- **** Command to fix corrupted index sequence ****
+
+CREATE TABLE public.example
+(
+    id   SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+insert into public.example (id, name)
+values (14, 'asdfasdfasdf');
+
+insert into public.example (name)
+values ('asdfasdfasdf');
+
+DROP SEQUENCE public.example_id_seq;-- <- could not work if the sequence is bounded to the objects.
+
 
 -- Execute then below command when the insertion error happens on serial column
 CREATE SEQUENCE public.example_id_seq1;
